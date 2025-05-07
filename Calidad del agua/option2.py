cf82aeee6bc99014f776c809dcaa6342c046864c
Neza-01 <hernandezcastronezareth@gmail.com>
2025-05-07 07:33:52 -0500

    Menu

---
+++ Calidad del agua/option2.py
@@ -0,0 +1,12 @@
+import os
+
+def limpiar():
+    os.system('cls')
+def pausa():
+    os.system("pause")
+
+def menuOp2():
+    print("Generacion de graficas.")
+    print("1.Tipo de grafica")
+    print("2. Elegir formato de ejemplo")
+    print("3. Atras")
